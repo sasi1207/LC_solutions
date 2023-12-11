3fa0ca8dc06e457c863ad7b889730f780fdf205c
sasi1207 <109613548+sasi1207@users.noreply.github.com>
2023-12-11 23:43:05 +0530

    Time: 44 ms (13.95%), Space: 20.8 MB (9.56%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int h=0,i=0,n=s.size();
+        string d="";
+        while(i<n){
+            if(d.find(s[i])==-1){
+                d+=s[i];
+                int f=d.size();
+                h=max(h,f);
+                cout<<d<<" "<<h<<endl;
+            }
+            else{
+                int g=d.find(s[i]);
+                d=d.substr(g+1);
+                d+=s[i];
+            }
+            i++;
+        }
+        return h;
+    }
+};
