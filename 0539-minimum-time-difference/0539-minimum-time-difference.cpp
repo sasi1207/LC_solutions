16779456ffa2d065f4fb7d9926e7572c5c9bc874
Kanupuri Sai Sasank <kssasank04@gmail.com>
2024-09-16 11:28:55 +0530

    Time: 12 ms (57.78%), Space: 17.4 MB (95.56%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -5,11 +5,16 @@ public:
         vector<int> m(n);
         for(int i=0;i<n;i++){
             m[i]=(((int(t[i][0] - 48) * 10)+ int(t[i][1]-48))*60)+((int(t[i][3] - 48) * 10)+ int(t[i][4]-48));
+            cout<<m[i]<<endl;
         }
         sort(m.begin(),m.end());
-        if(m[n-1]>720){
-            return min(m[1]-m[0],(1440-m[n-1]+m[0]));
+        int mini=1440;
+        for(int i=1;i<n;i++){
+            mini=min(mini,m[i]-m[i-1]);
         }
-        return m[1]-m[0];
+        if(m[n-1]>720){
+            return min(mini,(1440-m[n-1]+m[0]));
+        }
+        return mini;
     }
 };
